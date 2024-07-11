d854f274d03f0c921feaebd8b1c0ea6b6880ec5d
Md. Nazmul Islam Rumman <142523457+pomegstar@users.noreply.github.com>
2024-07-11 01:01:10 -0700

    Delete project 4/project4 directory

--- project 4/project4/network/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import User, Posts, Follow, Like
-
-admin.site.register(User)
-admin.site.register(Posts)
-admin.site.register(Follow)
-admin.site.register(Like)
